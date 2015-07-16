d0e7ea288ed8dbf7d1894de6a6030c3c11f95a05
Vienta <yongxingshu@foxmail.com>
2015-07-17 06:18:38 +0800

    add:新增了设置页面

--- kuafu/kuafu/src/Controller/KFSettingsViewController.swift
+++ kuafu/kuafu/src/Controller/KFSettingsViewController.swift
@@ -8,12 +8,22 @@
 
 import UIKit
 
-class KFSettingsViewController: UIViewController {
-
+class KFSettingsViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+   
+    @IBOutlet weak var tbvSettings: UITableView!
+    var settingsList: NSMutableArray!
+    // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = KF_BG_COLOR
         self.title = "KF_SETTINGS_CONTROLLER_TITLE".localized
+        
+        var settings =  [["section":"通用","values":[KF_SHAKE_CREATE_TASK]],
+                        ["section":"反馈","values":[KF_FEEDBACK,KF_EVALUATION]],
+                        ["section":"关于","values":[KF_ABOUT_KUAFU,KF_HISTORY_VERSION,KF_LISENCE]]]
+        
+        self.settingsList = NSMutableArray(array: settings)
+        self.tbvSettings.reloadData()
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +31,54 @@ class KFSettingsViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    // MARK: - IBActions
+    // MARK: - Private Methods
+    
+    // MARK: - Public Methods
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // MARK: - UITableViewDataSource && UITableViewDelegate
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return self.settingsList.count
     }
-    */
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        let elementInSectionDict: NSDictionary = self.settingsList.objectAtIndex(section) as! NSDictionary
+        let elementsInSection: NSArray = elementInSectionDict.objectForKey("values") as! NSArray
+        
+        return elementsInSection.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cellIdentifier = "settingsIdentifier"
+        var settingsCell: UITableViewCell! = tableView.dequeueReusableCellWithIdentifier(cellIdentifier) as? UITableViewCell
+        if (settingsCell == nil) {
+            settingsCell = UITableViewCell(style: .Value1, reuseIdentifier: cellIdentifier)
+            settingsCell.accessoryType = UITableViewCellAccessoryType.DisclosureIndicator
+        }
+        
+        let elementInSectionDict: NSDictionary = self.settingsList.objectAtIndex(indexPath.section) as! NSDictionary
+        let elementsInSection: NSArray = elementInSectionDict.objectForKey("values") as! NSArray
+        
+        let elementValueKey: String = elementsInSection.objectAtIndex(indexPath.row) as! String
+        
+        settingsCell.textLabel?.text = KF_SETTINGS[elementValueKey]
+        
+        if (elementValueKey == KF_SHAKE_CREATE_TASK) {
+            settingsCell.accessoryView = UISwitch()
+        }
+        
+        return settingsCell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+    }
+    
+    func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        let elementInSectionDict: NSDictionary = self.settingsList.objectAtIndex(section) as! NSDictionary
+        return elementInSectionDict.objectForKey("section") as? String
+    }
 
 }
--- kuafu/kuafu/src/Util/KFConstant.swift
+++ kuafu/kuafu/src/Util/KFConstant.swift
@@ -4,11 +4,11 @@
 //
 //  Created by Vienta on 15/6/6.
 //  Copyright (c) 2015年 www.vienta.me. All rights reserved.
-//
+//  后期此文件需要分
 
 import UIKit
 
-// MARK: --Macro--
+// MARK: - Macro
 let KF_THEME_COLOR:UIColor = UIColor(red: 0, green: 0.78, blue: 0, alpha: 1)
 let KF_ICON_BG_COLOR:UIColor = UIColor(red: 0.01, green: 0.01, blue: 0.01, alpha: 1)
 let KF_BG_COLOR:UIColor = UIColor(red:0.96, green:0.96, blue:0.96, alpha:1)
@@ -17,17 +17,33 @@ let KF_LINE_COLOR:UIColor = UIColor(red:0.85, green:0.85, blue:0.86, alpha:1)
 let DEVICE_WIDTH:CGFloat = UIScreen.mainScreen().bounds.width
 let DEVICE_HEIGHT:CGFloat = UIScreen.mainScreen().bounds.height
 
-// MARK: --Constant--
+// MARK: - Constant
 let KF_LOCAL_NOTIFICATION_CATEGORY_REMIND = "KF_REMIND_CATEGORY"
 let KF_LOCAL_NOTIFICATION_CATEGORY_COMPLETE = "KF_COMPLETE_CATEGORY"
 
-// MARK: --Notification--
+// MARK: - Notification
 let KF_NOTIFICATION_UPDATE_TASK = "KFNotificationUpdateTask"
 
-// MARK: --SettingsKey--
+// MARK: - SettingsKey
 let KF_EVENTKIT_ACCEES_GRANTED = "KFEventKitAccessGrandted"
 let KF_EVENTKIT_SELECTED_CALENDAR = "KFEventKitSelectedCalendar"
 let KF_EVENTKIT_CAL_IDENTIFIERS = "KFEventKitCalIdentifiers"
+let KF_SHAKE_CREATE_TASK = "KFShakeCreateTask"  //摇一摇新建任务
+let KF_FEEDBACK = "KFFeedBack"                  //意见反馈
+let KF_EVALUATION = "KFEvaluation"              //我要点评
+let KF_ABOUT_KUAFU = "KFAboutKuaFu"             //关于夸父
+let KF_HISTORY_VERSION = "KFHistoryVersion"     //历史版本
+let KF_LISENCE = "KFLisence"                    //开源许可
+
+
+// MARK: - Global Methods
+var KF_SETTINGS: Dictionary<String, String> = [
+    KF_SHAKE_CREATE_TASK:"KF_SETTINGS_SHAKE_NEW_TASK".localized,
+    KF_FEEDBACK:"KF_SETTINGS_FEEDBACK".localized,
+    KF_EVALUATION:"KF_SETTINGS_EVALUATION".localized,
+    KF_ABOUT_KUAFU:"KF_SETTINGS_ABOUT_KUAFU".localized,
+    KF_HISTORY_VERSION:"KF_SETTINGS_HISTORY_VERSION".localized,
+    KF_LISENCE:"KF_SETTINGS_LISENCE".localized]
 
 func SAFE_OBJC(objc: AnyObject!) -> AnyObject {
     if (objc == nil){
