9befd76ef5cfe45ccd9fd9a247eb38b0fda3e377
Vienta <yongxingshu@foxmail.com>
2015-08-17 23:10:48 +0800

    modify:修改explore页面

---
+++ kuafu/kuafu/src/Controller/explore/KFExploreViewController.swift
@@ -0,0 +1,35 @@
+//
+//  KFExploreViewController.swift
+//  kuafu
+//
+//  Created by Vienta on 15/8/17.
+//  Copyright (c) 2015年 www.vienta.me. All rights reserved.
+//
+
+import UIKit
+
+class KFExploreViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
