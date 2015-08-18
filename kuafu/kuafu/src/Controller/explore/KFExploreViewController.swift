3566629bed9d37d5776adce35a2b495f65fc978a
Vienta <yongxingshu@foxmail.com>
2015-08-18 22:33:42 +0800

    add:添加探索

--- kuafu/kuafu/src/Controller/explore/KFExploreViewController.swift
+++ kuafu/kuafu/src/Controller/explore/KFExploreViewController.swift
@@ -8,12 +8,20 @@
 
 import UIKit
 
-class KFExploreViewController: UIViewController {
+class KFExploreViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+
+    // MARK: - Property
+    @IBOutlet weak var tbvExplore: UITableView!
+    var exploreItem: Array<String>!
+    
+    // MARK: - Life Cycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        self.exploreItem = ["KF_ONE_KEY_RELEASE".localized]
+        self.title = "KF_EXPLORE_TITLE".localized
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +29,32 @@ class KFExploreViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // MARK: - IBActions
+    // MARK: - Private Methods
+    // MARK: - Public Methods
+    // MARK: - UITaleViewDelegate && UITableViewDataSource
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return self.exploreItem.count
     }
-    */
-
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cellIdentifier = "exploreIdentifier"
+        var cell: UITableViewCell? = tableView.dequeueReusableCellWithIdentifier(cellIdentifier) as? UITableViewCell
+        
+        if (cell == nil) {
+            cell = UITableViewCell(style: UITableViewCellStyle.Value1, reuseIdentifier: cellIdentifier)
+        }
+        cell?.textLabel?.text = self.exploreItem[indexPath.row] as String
+        
+        return cell!
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+    }    
 }
